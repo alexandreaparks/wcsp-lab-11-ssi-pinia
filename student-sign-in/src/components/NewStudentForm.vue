e4f38b81ed44ec084397f053e9dc78f84997c925
alexandrea parks <alparks@idk.com>
2024-04-09 17:10:31 -0500

    set up app, bootstrap, new student form

---
+++ student-sign-in/src/components/NewStudentForm.vue
@@ -0,0 +1,82 @@
+<script setup>
+
+import {ref} from "vue";
+
+const newStudentName = ref('')
+const newStarID = ref('')
+
+const errors = ref([])
+
+function addStudent() {
+
+  errors.value = []  // reset errors array
+
+  if (!newStudentName.value) {
+    errors.value.push('Enter name')
+  }
+  if (!newStarID.value) {
+    errors.value.push('Enter star ID')
+  }
+
+  if (errors.value.length === 0) {  // if no errors
+
+    // build a new student object
+    let student = {
+      name: newStudentName.value,
+      starID: newStarID.value,
+      present: false
+    }
+
+    // add to list
+    // TODO - how to add student
+
+    // clear both input fields
+    newStudentName.value = ''
+    newStarID.value = ''
+
+  }
+
+
+}
+
+
+
+</script>
+
+<template>
+
+  <div id="new-student-form-errors" class="m-2">
+    <!--show errors from form validation
+        if there are errors, show the div-->
+    <div v-if="errors.length > 0" class="alert alert-danger">
+      <li v-for="error in errors">
+        {{ error }}
+      </li>
+    </div>
+  </div>
+
+
+  <div id="new-student-form" class="card add-student m-2 p-2">
+    <h4 class="card-title">Add a new student</h4>
+
+    <div class="form-group mb-3">
+      <label for="name">Name</label>
+      <!-- v-model newStudentName -->
+      <input v-model.trim="newStudentName" id="name" class="form-control">
+    </div>
+
+    <div class="form-group mb-3">
+      <label for="starID">Star ID</label>
+      <!-- v-model newStarID -->
+      <input v-model.trim="newStarID" id="starID" class="form-control">
+    </div>
+
+    <!-- v-on:click event handler -->
+    <button v-on:click="addStudent" class="btn btn-primary">Add</button>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
